deea3e9fc90c2a722011d32231339d6ac899a7f4
Jacques Lucke <mail@jlucke.com>
2024-08-31 15:37:14 +0200

    add initial tests

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ use actix_cors::Cors;
 use actix_web::http::header::{CacheControl, CacheDirective, ContentType};
 use actix_web::http::StatusCode;
 use actix_web::middleware::DefaultHeaders;
-use actix_web::{error, get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{error, get, post, web, App, HttpResponse, HttpServer, Responder};
 use actix_web_httpauth::extractors::bearer::BearerAuth;
 use chrono::{DateTime, Utc};
 use clap::Parser;
 use derive_more::derive::{Display, Error};
 use include_dir::include_dir;
-use std::str::FromStr;
+use std::net::TcpListener;
 use std::time::Duration;
 use std::{
     collections::HashMap,
@@ -49,9 +49,9 @@ impl error::ResponseError for AppError {
         match *self {
             AppError::BadUserID => StatusCode::BAD_REQUEST,
             AppError::BadSessionID => StatusCode::BAD_REQUEST,
-            AppError::SessionIDDoesNotExist => StatusCode::BAD_REQUEST,
+            AppError::SessionIDDoesNotExist => StatusCode::NOT_FOUND,
             AppError::ServerError => StatusCode::INTERNAL_SERVER_ERROR,
-            AppError::BadAccessToken => StatusCode::BAD_REQUEST,
+            AppError::BadAccessToken => StatusCode::UNAUTHORIZED,
             AppError::PageTooLarge => StatusCode::PAYLOAD_TOO_LARGE,
         }
     }
@@ -92,7 +92,7 @@ struct AccessToken(String);
 
 impl AccessToken {
     fn from_string(s: &str) -> Result<AccessToken, AppError> {
-        if s.len() < 15 {
+        if s.len() < 10 {
             Err(AppError::BadAccessToken)
         } else if s.len() > 100 {
             Err(AppError::BadAccessToken)
@@ -270,12 +270,7 @@ async fn get_responses(
     }
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let args = Args::parse();
-
-    println!("Start server on http://{}:{}", args.host, args.port);
-
+async fn start_server(listener: TcpListener) -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(SharedState {
@@ -292,7 +287,184 @@ async fn main() -> std::io::Result<()> {
             .service(get_responses)
     })
     .workers(1)
-    .bind((args.host, args.port))?
+    .listen(listener)
+    .unwrap()
     .run()
     .await
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let mut args = Args::parse();
+
+    let listener = TcpListener::bind((args.host.clone(), args.port)).expect("Cannot bind to port");
+    let actual_port = listener.local_addr().unwrap().port();
+    args.port = actual_port;
+
+    println!("Start server on http://{}:{}", args.host, actual_port);
+
+    start_server(listener).await
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct TestContext {
+        handle: tokio::task::JoinHandle<()>,
+        url: String,
+        client: reqwest::Client,
+    }
+
+    impl Drop for TestContext {
+        fn drop(&mut self) {
+            self.handle.abort();
+        }
+    }
+
+    impl TestContext {
+        async fn request_session_page_text(&self, session_id: &str) -> String {
+            self.request_session_page(session_id)
+                .await
+                .text()
+                .await
+                .unwrap()
+        }
+
+        async fn request_session_page(&self, session_id: &str) -> reqwest::Response {
+            self.client
+                .get(format!("{}?session={}", &self.url, &session_id))
+                .send()
+                .await
+                .unwrap()
+        }
+
+        async fn request_page_update(
+            &self,
+            session_id: Option<&str>,
+            token: Option<&str>,
+            page: &str,
+        ) -> reqwest::Response {
+            let url = match session_id {
+                None => format!("{}/set_page", &self.url),
+                Some(session_id) => format!("{}/set_page?session={}", &self.url, session_id),
+            };
+            let mut builder = self.client.post(&url);
+            if token.is_some() {
+                builder = builder.bearer_auth(token.unwrap());
+            }
+            builder = builder.body(page.to_string());
+            builder.send().await.unwrap()
+        }
+
+        async fn request_static(&self, path: &str) -> reqwest::Response {
+            self.client
+                .get(format!("{}{}", self.url, path))
+                .send()
+                .await
+                .unwrap()
+        }
+    }
+
+    async fn setup() -> TestContext {
+        let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+        let port = listener.local_addr().unwrap().port();
+
+        let server = tokio::spawn(async move {
+            start_server(listener)
+                .await
+                .expect("failed to start server");
+        });
+
+        // Wait for server to start.
+        tokio::time::sleep(Duration::from_millis(100)).await;
+
+        TestContext {
+            handle: server,
+            url: format!("http://127.0.0.1:{}", port),
+            client: reqwest::Client::new(),
+        }
+    }
+
+    #[tokio::test]
+    async fn static_index_page() {
+        let ctx = setup().await;
+        let res = ctx.request_static("/").await;
+        assert_eq!(res.text().await.unwrap(), get_static_file("index.html"));
+    }
+
+    #[tokio::test]
+    async fn not_found_session_page() {
+        let ctx = setup().await;
+        let res = ctx.request_session_page("1").await;
+        assert_eq!(res.status(), reqwest::StatusCode::NOT_FOUND);
+    }
+
+    #[tokio::test]
+    async fn set_page_without_token() {
+        let ctx = setup().await;
+        let res = ctx.request_page_update(Some("1"), None, "test").await;
+        assert_eq!(res.status(), reqwest::StatusCode::UNAUTHORIZED);
+    }
+
+    #[tokio::test]
+    async fn set_page_and_request() {
+        let ctx = setup().await;
+
+        let page = "my test page";
+
+        let res = ctx
+            .request_page_update(Some("1"), Some("my-test-token"), page)
+            .await;
+        assert_eq!(res.status(), reqwest::StatusCode::OK);
+
+        let res = ctx.request_session_page("1").await;
+        assert_eq!(res.status(), reqwest::StatusCode::OK);
+        assert_eq!(res.text().await.unwrap(), page);
+    }
+
+    #[tokio::test]
+    async fn set_page_twice_with_same_token() {
+        let ctx = setup().await;
+
+        let session = "a";
+        let token = "my-test-token";
+        let page_1 = "page one";
+        let page_2 = "page two";
+
+        let res = ctx
+            .request_page_update(Some(session), Some(token), "page one")
+            .await;
+        assert_eq!(res.status(), reqwest::StatusCode::OK);
+        assert_eq!(ctx.request_session_page_text(session).await, page_1);
+
+        let res = ctx
+            .request_page_update(Some(session), Some(token), page_2)
+            .await;
+        assert_eq!(res.status(), reqwest::StatusCode::OK);
+        assert_eq!(ctx.request_session_page_text(session).await, page_2);
+    }
+
+    #[tokio::test]
+    async fn try_update_page_with_other_token() {
+        let ctx = setup().await;
+
+        let session = "b";
+        let token_1 = "my-first-token";
+        let token_2 = "my-second-token";
+        let page_1 = "page 1";
+        let page_2 = "page 2";
+
+        let res = ctx
+            .request_page_update(Some(session), Some(token_1), page_1)
+            .await;
+        assert_eq!(res.status(), reqwest::StatusCode::OK);
+        assert_eq!(ctx.request_session_page_text(session).await, page_1);
+
+        let res = ctx
+            .request_page_update(Some(session), Some(token_2), page_2)
+            .await;
+        assert_eq!(res.status(), reqwest::StatusCode::UNAUTHORIZED);
+        assert_eq!(ctx.request_session_page_text(session).await, page_1);
+    }
 }
